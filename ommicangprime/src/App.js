06a2e896937280c91dbc8f05f574992144e7ce24
OMMICANG <ommicang@gmail.com>
2024-08-06 04:18:15 +0100

    Route Updates++

--- ommicangprime/src/App.js
+++ ommicangprime/src/App.js
@@ -45,7 +45,7 @@ function App() {
             <div>
                 {loading ? <Preloader /> : (
                     <Routes>
-                        <Route path="/ommicangprime" element={<LandingPage />}/>
+                        <Route path="/" element={<LandingPage />}/>
                         <Route path="/auth" element={<AuthPage />}/>
                     </Routes>
                 )}
