5e2881011273c84c483d58c51cf365b4436c5032
OMMICANG <ommicang@gmail.com>
2024-08-06 02:32:34 +0100

    updates test

--- ommicangprime/src/App.js
+++ ommicangprime/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'; // Ensure you have this file for custom styles
+import Preloader from './preloader'; //Import the Preloader component
+import LandingPage from './landingPage'; //import the landingPage component
+import AuthPage from './AuthPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loading, setLoading] = useState(true);
+
+    
+
+  useEffect(() => {
+    const handleTelegramInit = () => {
+
+        const webapp = window.Telegram.WebApp;
+
+        webapp.ready(); // Initialize The Web App
+
+        webapp.expand(); // Expand The Web App To Full Height
+
+           // Simulate a loading delay for demonstration purposes
+           setTimeout(() => {
+            setLoading(false);
+        }, 2000); // Adjust the delay as needed
+
+    };
+
+    // Ensure the Telegram Web App SDK is loaded before trying to initialize
+    if (window.Telegram) {
+        handleTelegramInit();
+    } else {
+        document.addEventListener('TelegramWebAppReady', handleTelegramInit);
+    }
+
+    return () => {
+        document.removeEventListener('TelegramWebAppReady', handleTelegramInit);
+    };
+}, []);
+
+    return (
+
+        <Router>
+
+            <div>
+                {loading ? <Preloader /> : (
+                    <Routes>
+                        <Route path="/ommicang" element={<LandingPage />}/>
+                        <Route path="/auth" element={<AuthPage />}/>
+                    </Routes>
+                )}
+            </div>
+        </Router>
+    );
 }
 
 export default App;
