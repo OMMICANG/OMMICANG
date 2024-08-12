b71bffe9f5ae85a5eefc872021162f7566de1301
OMMICANG <ommicang@gmail.com>
2024-08-12 11:28:39 +0100

    sever test Updates++

---
+++ ommicang/src/HomePage.js
@@ -0,0 +1,14 @@
+// src/HomePage.js
+
+import React from 'react';
+import './HomePage.css'; // Ensure you have this file for custom styles
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            {/* Content for the homepage can be added here */}
+        </div>
+    );
+};
+
+export default HomePage;
